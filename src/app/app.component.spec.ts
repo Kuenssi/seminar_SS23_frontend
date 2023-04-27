39739561ab99de1b4efdcf9286f72944c800dbc2
Kuenssi <max.kuenssi@gmail.com>
2023-04-27 14:21:34 +0200

    chore: change project name

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'seminarW22_frontend'`, () => {
+  it(`should have as title 'seminarSS23_frontend'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('seminarW22_frontend');
+    expect(app.title).toEqual('seminarSS23_frontend');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('seminarW22_frontend app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('seminarSS23_frontend app is running!');
   });
 });
